8038a053d29675635435098e9cba1c2d433b93b9
abeciana1 <alex.beciana@gmail.com>
2020-06-27 16:06:44 -0400

    First Commit

--- restaurant_ruby.rb
+++ restaurant_ruby.rb
@@ -1,15 +1,23 @@
 require 'pry'
+require './restaurant_ruby_2.rb'
 
 class Restaurant
 
-    attr_accessor :name, :star_rating
+    attr_accessor :name, :star_rating, :owner
+
+    @@all = []
 
     def initialize(name, star_rating)
         @name = name
         @star_rating = star_rating
+        @all << self
     end
+
+    def 
     
 end
+
+puts alex = RestaurantOwner.new("Alex", 25)
 
 #binding.pry
 
